0a746bab593e1cf9b3a5d4316e78e25be47ddb03
Rafael Valiev <feelermorses@gmail.com>
2018-08-19 14:46:49 +0300

    SpringBootApplication run

---
+++ src/main/java/ru/dobrotrener/spring5mvctest/Spring5MvcTestApplication.java
@@ -0,0 +1,12 @@
+package ru.dobrotrener.spring5mvctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5MvcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5MvcTestApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/dobrotrener/spring5mvctest/Spring5MvcTestApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.dobrotrener.spring5mvctest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5MvcTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
